4e0fce36fec680336aef627bf34d32bc804c5593
shawn <lxhbit@gmail.com>
2021-01-26 20:01:47 +0100

    change training shell

--- scripts/points_signle_gpu_train_waymo.sh
+++ scripts/points_signle_gpu_train_waymo.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$PWD:$PYTHONPATH
 
-python main.py configs/point_within_lidar_template.yaml --ngpus 1 --world-size 1 
+python main.py configs/point_within_lidar_template.yaml --multiprocessing-distributed --rank 0 --ngpus 1 --world-size 1 
